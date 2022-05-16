d734e4265c7b3e3c570162aa71e242add72167be
jeong jun <jungjunjenon3047@gmail.com>
2022-05-17 03:16:09 +0900

    initial commit

---
+++ nav.js
@@ -0,0 +1,22 @@
+const openMeun = () => {
+    document.querySelector('.backdrop').className = 'backdrop active';
+    document.querySelector('aside').className = 'active';
+}
+
+const closeMeun = () => {
+    document.querySelector('.backdrop').className = 'backdrop';
+    document.querySelector('aside').className = '';
+}
+
+
+document.getElementById('meun').onclick = e => {
+    openMeun();
+}
+
+document.querySelector('aside .close').onclick = e => {
+    closeMeun();
+}
+
+document.querySelector('.backdrop').onclick = e => {
+    closeMeun();
+}
